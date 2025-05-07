b76dd22993959db8bf2887af77b40abadca4b254
juliaapolonio <apoloniojulia@gmail.com>
2025-05-07 14:18:02 -0300

    remove time counting inside R

--- bin/run_scrank.R
+++ bin/run_scrank.R
@@ -14,14 +14,8 @@ cell_type <- sub(".RDS", "", seuratObj)
 
 sc_obj <- readRDS(seuratObj)
 
-start_time <- Sys.time()
-
 weight <- GENIE3(as.matrix(sc_obj@data$seuratObj[sc_obj@para$gene4use]@assays$RNA$counts), nCores = n_cores)
 weight <- weight[colnames(weight),]
-
-end_time <- Sys.time()
-elapsed_time <- end_time - start_time
-print(elapsed_time)
 
 n_cells <- dim(sc_obj@data$seuratObj)[2]
 
